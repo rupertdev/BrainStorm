cf74d46e7572cc799b7cc3ec390ef59e0f88a057
Stefan Kraus <stefan.kraus@workiva.com>
2016-09-17 13:54:48 -0500

    Login with google

--- client/js/components/loginPage.js
+++ client/js/components/loginPage.js
@@ -1,11 +1,50 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var MaterialUI = require('material-ui');
+var store = require('../store');
 
 var loginPage = React.createClass({
-  render: function(){
-    return React.createElement('div', {}, 'ur trash. no enterino.');
+  getInitialState: function() {
+    return {
+      logged: false
+    };
+  },
+
+  onSignIn: function(googleUser) {
+    console.log("user signed in:"); // plus any other logic here
+    this.setState({logged: true});
+  },
+
+  renderGoogleLoginButton: function() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'https://www.googleapis.com/auth/plus.login',
+      'width': 220,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'light',
+      'onsuccess': this.onSignIn
+    })
+  },
+
+  componentDidMount: function() {
+    window.addEventListener('google-loaded', this.renderGoogleLoginButton);
+  },
+
+  render: function() {
+    let displayText = "Sign in with Google";
+    return (
+        React.createElement(
+            'div',
+            {'id': 'my-signin2',
+            'style': {
+              'display': this.state.logged ? 'none' : '',
+              'margin': 'auto',
+              'width': '200px'
+            }
+            })
+    );
   }
+
 });
 
 module.exports = loginPage;
--- client/js/reducer.js
+++ client/js/reducer.js
@@ -1,10 +1,10 @@
-function counter(state = false, action) {
-  switch (action.type) {
-  case 'LEFT_PANEL_TOGGLE':
-    return !state;
-  default:
-    return state
+function loggedIn(state = false, action){
+  switch(action.type) {
+    case 'success':
+      return !state;
+    default:
+      return state;
   }
 }
 
-module.exports = counter;
+module.exports = loggedIn;
