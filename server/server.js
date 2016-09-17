f788c19dd6fcbb62d5a2bff6b1054837d0765b81
Nickolas.Wilson <Nickolas.Wilson@workiva.com>
2016-09-16 20:23:58 -0500

    Client routing: - Added /client/index.html file - Added static dir express setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 //Link dependencies
+var path = require('path');
 var express = require('express');
 var config = require('./config.json');
 
 //Setup server
 var app = express();
+app.use(express.static(path.join(__dirname, '../', 'client'), {
+    extensions: ['html'],
+    index: 'index.html'
+}));
 
 //Middleware definitions
 var logging = function (req, res, next) {
